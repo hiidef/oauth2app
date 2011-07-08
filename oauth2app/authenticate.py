e82c92d0e65a817f03c55311b84d8d7816f9eda4
John Wehr <johnwehr@gmail.com>
2011-07-08 18:58:02 -0400

    MAC authentication works. Still unfinished.

--- oauth2app/authenticate.py
+++ oauth2app/authenticate.py
@@ -147,7 +147,7 @@ class Authenticator(object):
             raise InvalidToken("MAC authentication is not supported.")
         mac_header = parse_qsl(mac_header.replace(",","&").replace('"', ''))
         mac_header = dict([(x[0].strip(), x[1].strip()) for x in mac_header])
-        for parameter in ["id", "nonce", "mac"]
+        for parameter in ["id", "nonce", "mac"]:
             if "parameter" not in mac_header:
                 raise InvalidToken("MAC Authorization header does not contain"
                     " required parameter '%s'" % parameter)
@@ -171,7 +171,9 @@ class Authenticator(object):
             self.request_hostname, # The hostname included in the HTTP request
             self.request_port, # The port as included in the HTTP request
             bodyhash,
-            ext]).hexdigest()
+            ext])).hexdigest()
+        raise NotImplementedError()
+
         # Todo:
         # 1.  Recalculate the request body hash (if included in the request) as
         # described in Section 3.2 and request MAC as described in
@@ -183,7 +185,7 @@ class Authenticator(object):
         # the determination of staleness is left up to the server to
         # define).
         # 3.  Verify the scope and validity of the MAC credentials.
-        raise NotImplementedError()
+        
 
     def _get_user(self):
         """The user associated with the valid access token.
