a73f37336d1995791087854070ad38cf70b5df88
John Wehr <johnwehr@gmail.com>
2011-07-12 06:45:40 -0400

    Docs updates.

--- docs/conf.py
+++ docs/conf.py
@@ -99,9 +99,9 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 if on_rtd:
-    html_theme = 'hiidef'
-else:
     html_theme = 'default'
+else:
+    html_theme = 'hiidef'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
