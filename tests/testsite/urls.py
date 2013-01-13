834977793c71db1ed301fe6340b69ffe5dbaf840
Robert Coup <robert@coup.net.nz>
2013-01-13 19:02:28 +1300

    Fix test failures

--- tests/testsite/urls.py
+++ tests/testsite/urls.py
@@ -4,5 +4,4 @@ from django.conf import settings
 urlpatterns = patterns('',
     (r'^oauth2/', include('testsite.apps.oauth2.urls')),
     (r'^api/', include('testsite.apps.api.urls')),
-    (r'^account/', include('testsite.apps.account.urls')),
 )
