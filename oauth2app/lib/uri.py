6c8918f2f24ddd5dc500e31b37f4f52e61e3280f
Mitsukuni Sato <mitsukuni.sato@gmail.com>
2013-07-30 01:38:17 +0900

    add //

--- oauth2app/lib/uri.py
+++ oauth2app/lib/uri.py
@@ -4,7 +4,7 @@
 """OAuth 2.0 URI Helper Functions"""
 
 
-from urlparse import urlparse, urlunparse, parse_qsl
+from urlparse import urlparse, urlunparse, parse_qsl, uses_netloc
 from urllib import urlencode
 from url_normalize import url_normalize
  
@@ -19,9 +19,12 @@ def add_parameters(url, parameters):
 
     *Returns str*"""
     parts = list(urlparse(url))
-    parts[4] = urlencode(parse_qsl(parts[4]) + parameters.items())
-    return urlunparse(parts)
-    
+    if parts[0] in uses_netloc:
+        parts[4] = urlencode(parse_qsl(parts[4]) + parameters.items())
+        return urlunparse(parts)
+    else:
+        return '%s?%s' % (url, urlencode(parse_qsl(parts[4]) + parameters.items()))
+
 
 def add_fragments(url, fragments):
     """Parses URL and appends fragments. 
@@ -33,9 +36,12 @@ def add_fragments(url, fragments):
 
     *Returns str*"""
     parts = list(urlparse(url))
-    parts[5] = urlencode(parse_qsl(parts[5]) + fragments.items())
-    return urlunparse(parts)
-    
+    if parts[0] in uses_netloc:
+        parts[5] = urlencode(parse_qsl(parts[5]) + fragments.items())
+        return urlunparse(parts)
+    else:
+        return '%s#%s' % (url, urlencode(parse_qsl(parts[5]) + fragments.items()))
+
     
 def normalize(url):
     """Normalizes URL.
